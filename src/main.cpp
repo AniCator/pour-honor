53e7501dca2fc803dc45514a11f7d73b2fcd77fd
AniCator <contact@anicator.com>
2022-08-27 16:32:16 +0200

    Set up the possible game states for this jam.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "olcPixelGameEngine.h"
 #include "olcSoundWaveEngine.h"
-#include "AssetManager.h"
 
+#include "AssetManager.h"
 using Assets = AssetManager;
+
+// Disable the console window on Windows.
+#define ConsoleWindowDisabled
+#if defined( ConsoleWindowDisabled )
+#if defined( _MSC_VER )
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+#endif
 
 class PourHonor : public olc::PixelGameEngine
 {
@@ -12,17 +20,27 @@ public:
         sAppName = "PourHonor";
     }
 
-private:
-    olc::sound::WaveEngine WaveEngine;
-    int BackgroundLayer = -1;
+    enum GameState
+    {
+	    Title, // Title card that waits for user input. (Press Enter to begin)
+        Intro, // Intro that is played after the title card. (skip using Spacebar)
 
-public:
+        Observe, // Stare out the window and try to dicern what the weather will be.
+        Predict, // Submit predictions to the computer.
+        Presentation, // Your predictions are presented on the television.
+        Reality, // Were you indeed correct?
+        Consequences, // The senate will decide your fate.
+
+        Lose, // You're fired, your reputation is in the dumps.
+        Win, // You're the greatest weatherman that has ever existed, good on you buddy!
+    } State;
+
     bool OnUserCreate() override
     {
         WaveEngine.InitialiseAudio();
         Assets::LoadGraphic( "background", "assets/gfx/space.png" );
 
-        Assets::LoadSound( "bg-music", "assets/sounds/bg-music.wav" );
+        Assets::LoadSound( "bg-music", "assets/sounds/TheHaedooLow11250.wav" );
         Assets::LoadSound( "laser", "assets/sounds/Laser_Shoot11.wav" );
         Assets::LoadSound( "explosion", "assets/sounds/Explosions1.wav" );
         Assets::LoadSound( "lose", "assets/sounds/lose9.wav" );
@@ -35,26 +53,30 @@ public:
         DrawSprite( 0, 0, Assets::GetSprite( "background" ) );
         SetDrawTarget( nullptr );
 
-        WaveEngine.PlayWaveform( Assets::GetSound( "bg-music" ), true );
+        auto Wave = WaveEngine.PlayWaveform( Assets::GetSound( "bg-music" ), true );
 
         return true;
     }
 
-    // Called by olcConsoleGameEngine
     bool OnUserUpdate( float DeltaTime ) override
     {
         Clear( olc::BLANK );
 
+        DrawLine( 0, 0, 320, 180 );
+
         return !GetKey( olc::ESCAPE ).bPressed;
     }
+
+    olc::sound::WaveEngine WaveEngine;
+    int BackgroundLayer = -1;
 };
 
 
 int main()
 {
-    PourHonor game;
-    if( game.Construct( 320, 180, 4, 4 ) )
-        game.Start();
+    PourHonor Game;
+    if( Game.Construct( 320, 180, 2, 2 ) )
+        Game.Start();
 
     return 0;
 }
