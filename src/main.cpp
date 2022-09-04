fa65c9c1d962168d24582dbdd37d6ad23026ce00
AniCator <contact@anicator.com>
2022-09-04 18:30:57 +0200

    The prediction mechanics.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ using Assets = AssetManager;
 #endif
 #endif
 
+// #define DisableAudio
+
 namespace Palette
 {
     struct Palette
@@ -97,6 +99,53 @@ public:
 
     olc::Sprite* WeatherSprite = nullptr;
 
+    enum class WeatherType : uint8_t
+    {
+	    Clear = 0,
+        Cloudy,
+        Rain,
+
+        Invalid,
+
+        Maximum
+    };
+
+    std::vector<std::string> WeatherString = {
+        "Clear",
+        "Cloudy",
+        "Rain",
+        "Invalid"
+    };
+
+    WeatherType Prediction = WeatherType::Invalid;
+
+    const std::string& GetWeatherLabel( const WeatherType Type )
+    {
+	    if( Type == WeatherType::Maximum )
+	    {
+            static std::string Invalid = "Invalid";
+            return Invalid;
+	    }
+
+        return WeatherString[static_cast<uint8_t>( Type )];
+    }
+
+    struct WeatherRecord
+    {
+        int Offset = 0;
+        WeatherType Type = WeatherType::Clear;
+    };
+
+    std::vector<WeatherRecord> WeatherData = {
+        {-320,WeatherType::Clear},
+        {10,WeatherType::Cloudy},
+        {200,WeatherType::Clear},
+        {370,WeatherType::Rain},
+        {830,WeatherType::Clear},
+        {1100,WeatherType::Cloudy},
+        {1210,WeatherType::Clear}
+    };
+
     struct IntroCard
     {
         float Duration = 3.0f;
@@ -126,7 +175,9 @@ public:
 
     bool OnUserCreate() override
     {
-        WaveEngine.InitialiseAudio( 44100, 2, 8, 2048 );
+#ifndef DisableAudio
+        WaveEngine.InitialiseAudio( 44100, 2, 4, 16384 );
+#endif
 
         // Load sounds.
         Assets::LoadSound( "music", "assets/sounds/cator_weather_channel.wav" );
@@ -260,6 +311,9 @@ public:
 
         Clear( olc::BLANK );
 
+        // Call it every update.
+        std::rand();
+
         const auto ValidState = UpdateState();
         if( !ValidState )
         {
@@ -292,6 +346,7 @@ public:
 
     void PlayBackgroundSound( olc::sound::Wave* Pointer, const bool Loop = true )
     {
+#ifndef DisableAudio
         if( !Pointer )
             return;
 
@@ -302,6 +357,7 @@ public:
 
         BackgroundSound = WaveEngine.PlayWaveform( Pointer, Loop );
         HasPlayedBackgroundSoundAtLeastOnce = Loop;
+#endif
     }
 
     void UpdateTitle()
@@ -310,6 +366,10 @@ public:
         DrawString( 130, 60, "Pour Honor", Palette.Bright );
         DrawLine( 125, 70, 210, 70, Palette.Dark );
         DrawString( 90, 100, "Press Enter To Start", Palette.Bright );
+
+#ifdef DisableAudio
+        DrawString( 10, 150, "Audio is disabled for the web version.", Palette.Dark );
+#endif
 
         if( GetKey( olc::ENTER ).bReleased )
         {
@@ -347,7 +407,9 @@ public:
             CardIndex = 0;
             CardStartTime = TimeSinceStateChange();
 
+#ifndef DisableAudio
             BackgroundMusic = WaveEngine.PlayWaveform( Assets::GetSound( "music_intro" ), true );
+#endif
         }
 
         // Check if we can still be in this state.
@@ -371,6 +433,35 @@ public:
         }
     }
 
+    int WeatherOffset = 0;
+    size_t WeatherIndex = 0;
+    float StartOffset = 0.0f;
+    WeatherType PreviousWeather = WeatherType::Clear;
+    WeatherType CurrentWeather = WeatherType::Clear;
+    void UpdateCurrentWeather( const int Offset )
+    {
+        size_t Index = 0;
+        WeatherType Type = WeatherType::Clear;
+	    for( const auto& Weather : WeatherData )
+	    {
+            if( Offset < Weather.Offset )
+                break;
+
+            Type = Weather.Type;
+            Index++;
+	    }
+
+        PreviousWeather = CurrentWeather;
+        CurrentWeather = Type;
+        WeatherIndex = Index - 1;
+    }
+
+    WeatherType GetNextWeather()
+    {
+        const auto NextIndex = ( WeatherIndex + 1 ) % ( WeatherData.size() );
+        return WeatherData[NextIndex].Type;
+    }
+
     void RenderWeatherLayer()
     {
         SetDrawTarget( WeatherLayer );
@@ -378,11 +469,46 @@ public:
         {
             // Clear to sky background.
             Clear( olc::Pixel( 172, 115, 0 ) );
-            const auto WrapAround = WeatherSprite->width - 320;
-            const auto Offset = ( static_cast<int32_t>( TimeSinceStateChange() * -10.0f ) + 320 ) % WrapAround;
-            DrawSprite( Offset, 0, WeatherSprite );
+            const auto WrapAround = WeatherSprite->width - 100;
+            WeatherOffset = ( static_cast<int32_t>( TimeSinceStateChange() * -10.0f - StartOffset ) + 100 ) % WrapAround;
+            DrawSprite( WeatherOffset, 0, WeatherSprite );
+            WeatherOffset *= -1;
         }
         SetDrawTarget( nullptr );
+    }
+
+    bool AllowDebug = false;
+    void DebugWeather()
+    {
+        if( GetKey( olc::Key::HOME ).bReleased )
+        {
+            AllowDebug = !AllowDebug;
+        }
+
+        if( !AllowDebug )
+            return;
+
+        std::string Text = "Current Weather: ";
+        // Have to append because string_view doesn't support concatenation using the + operator.
+        Text += GetWeatherLabel( CurrentWeather );
+
+        Text += "\nNext: ";
+        Text += GetWeatherLabel( GetNextWeather() );
+
+        Text += "\nPrediction: ";
+        Text += GetWeatherLabel( Prediction );
+
+        Text += "\nOffset: " + std::to_string( WeatherOffset );
+
+        const auto NextIndex = ( WeatherIndex + 1 ) % ( WeatherData.size() );
+        Text += "\nIndex: " + std::to_string( WeatherIndex );
+        Text += "\nNextIndex: " + std::to_string( NextIndex );
+
+        SetPixelMode( olc::Pixel::ALPHA );
+        auto Color = Palette::Blue.Bright;
+        Color.a = 100;
+        DrawString( 100, 50, Text, Color );
+        SetPixelMode( olc::Pixel::NORMAL );
     }
 
     void UpdateObserve()
@@ -395,6 +521,17 @@ public:
         }
 
         RenderWeatherLayer();
+        UpdateCurrentWeather( WeatherOffset );
+
+        if( CurrentWeather != PreviousWeather )
+        {
+            if( Reputation > 0 )
+            {
+                Reputation--;
+            }
+        }
+
+        DebugWeather();
 
         EnableLayer( WeatherLayer, true );
         EnableLayer( WindowLayer, true );
@@ -409,8 +546,73 @@ public:
         if( !StateJustChanged )
             return;
 
+#ifndef DisableAudio
         WaveEngine.StopWaveform( BackgroundMusic );
         BackgroundMusic = WaveEngine.PlayWaveform( Assets::GetSound( "music" ), true );
+#endif
+    }
+
+    bool MouseInBox( const int32_t X, const int32_t Y, const int32_t W, const int32_t H ) const
+    {
+        const int32_t MouseX = GetMouseX();
+        const int32_t MouseY = GetMouseY();
+        const int32_t X2 = X + W;
+        const int32_t Y2 = Y + H;
+
+        if( MouseX >= X && MouseX <= X2 && MouseY >= Y && MouseY <= Y2 )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void RenderPredictionOptions()
+    {
+        const auto MouseDown = GetMouse( 0 ).bPressed;
+
+        size_t LabelIndex = 0;
+        WeatherType HoveredPrediction = WeatherType::Invalid;
+	    for( const auto& Label : WeatherString )
+	    {
+            if( LabelIndex >= static_cast<size_t>( WeatherType::Invalid ) )
+                break;
+
+            const int32_t X = 10;
+            const int32_t Y = 50 + LabelIndex * 20;
+            const int32_t W = 300;
+            const int32_t H = 20;
+
+            const auto HasMouseOver = MouseInBox( X, Y, W, H );
+            auto RectangleColor = MouseDown && HasMouseOver ? Palette.Neutral : HasMouseOver ? Palette.Dark : Palette.Darkest;
+            RectangleColor.a = 200;
+
+            SetPixelMode( olc::Pixel::ALPHA );
+            FillRect( X, Y, W, H, RectangleColor );
+            SetPixelMode( olc::Pixel::NORMAL );
+
+            // Outline
+            DrawRect( X, Y, W, H, Palette.Bright );
+
+            DrawString( 160 - ( Label.length() / 2 ) * 10, Y + 2, Label, HasMouseOver ? Palette.Bright : Palette.Neutral );
+
+            if( HasMouseOver )
+            {
+                HoveredPrediction = static_cast<WeatherType>( LabelIndex );
+            }
+
+            LabelIndex++;
+	    }
+
+        if( AllowDebug )
+			DrawString( 100, 30, GetWeatherLabel( HoveredPrediction ), Palette::Blue.Bright );
+
+        const auto MouseReleased = GetMouse( 0 ).bReleased;
+        if( MouseReleased && HoveredPrediction != WeatherType::Invalid )
+        {
+            Prediction = HoveredPrediction;
+            State = Presentation;
+        }
     }
 
     olc::sound::PlayingWave InteractionSound;
@@ -421,8 +623,14 @@ public:
 
         if( AnyKey() )
         {
+#ifndef DisableAudio
             InteractionSound = WaveEngine.PlayWaveform( Assets::GetSound( "computer_prediction" ) );
+#endif
         }
+
+        RenderPredictionOptions();
+
+        DebugWeather();
 
         if( GetKey( olc::ENTER ).bReleased )
         {
@@ -432,6 +640,9 @@ public:
         if( !StateJustChanged )
             return;
 
+#ifndef DisableAudio
+        WaveEngine.StopWaveform( BackgroundMusic );
+#endif
         PlayBackgroundSound( Assets::GetSound( "computer_idle" ) );
     }
 
@@ -442,8 +653,12 @@ public:
 
         if( AnyKey() )
         {
+#ifndef DisableAudio
             InteractionSound = WaveEngine.PlayWaveform( Assets::GetSound( "speech" ) );
+#endif
         }
+
+        DebugWeather();
 
         if( TimeSinceStateChange() > 5.0f )
         {
@@ -460,28 +675,122 @@ public:
     {
         const auto Duration = TimeSinceStateChange();
         EnableLayer( WeatherLayer, true );
-        SetLayerOffset( WeatherLayer, Duration * 0.1 - 1.0f, 0.0f );
-        DrawString( 0, 0, "This looks like what I expected..\nOr wait? What's that!?", Palette.Bright );
+
+        const auto CorrectPrediction = Prediction == GetNextWeather();
+        if( TimeSinceStateChange() > 3.0 )
+        {
+            if( CorrectPrediction )
+            {
+                DrawString( 0, 0, "Looking good!", Palette.Bright );
+            }
+            else
+            {
+                DrawString( 0, 0, "Wait.. What's that!?", Palette.Bright );
+            }
+        }
+        else if ( TimeSinceStateChange() > 1.0 )
+        {
+            DrawString( 0, 0, "Hmm..", Palette.Bright );
+        }
 
         if( Duration > 7.0f )
         {
             State = Consequences;
         }
 
+        DebugWeather();
+
         if( !StateJustChanged )
             return;
 
-        WaveEngine.StopWaveform( BackgroundMusic );
+        uint16_t NewReputation = Reputation;
+        if( CorrectPrediction )
+        {
+            NewReputation += 10;
+        }
+        else
+        {
+            NewReputation -= 11;
+        }
+
+        if( NewReputation > 100 )
+        {
+            NewReputation = 100;
+        }
+
+        if( NewReputation < 0 )
+        {
+            NewReputation = 0;
+        }
+
+        Reputation = static_cast<uint8_t>( NewReputation );
+
+        StartOffset = static_cast<float>( std::rand() ) / static_cast<float>( RAND_MAX );
+        StartOffset *= 100000.0f;
+
         PlayBackgroundSound( Assets::GetSound( "reality_sets_in" ), false );
     }
 
+    std::vector<std::string> ConsequencePositive = {
+        "Your boss awarded you\nanother cup of coffee.",
+        "You have been invited to a talk show.",
+        "Your son loves you.",
+        "You got a free lottery ticket!",
+        "Free donuts at your desk.",
+        "Your favourite meal is being plated.",
+        "Movie night with the wife!",
+        "There's a brand new car outside!",
+        "Time for a Hawaii cruise!",
+        "Sedit returned to the OLC server!"
+    };
+
+    std::vector<std::string> ConsequenceNegative = {
+        "There's an angry mob outside.",
+        "You got a papercut.",
+        "Your lottery ticket is worthless.",
+        "Your wife knows about your mistress.",
+        "So you chose this as your career, huh?",
+        "Fanmail has stopped arriving.",
+        "#terribleweatherman is trending.",
+        "A comic book villain is based on you.",
+        "The trams are running late.",
+        "Someone else predicted the weather\nbetter than you did!?"
+    };
+
+    std::string Consequence;
+
     void UpdateConsequences()
     {
-        DrawString( 0, 0, "Well, well, well...\nIf it isn't the consequences\nof my actions.", Palette.Bright );
+        const auto CorrectPrediction = Prediction == GetNextWeather();
+
+        if( StateJustChanged )
+        {
+            if( CorrectPrediction )
+            {
+                Consequence = ConsequencePositive[std::rand() % ( ConsequencePositive.size() )];
+            }
+            else
+            {
+                Consequence = ConsequenceNegative[std::rand() % ( ConsequenceNegative.size() )];
+            }
+        }
+
+        DrawString( 10, 50, Consequence, Palette.Bright );
 
         if( TimeSinceStateChange() > 6.0f )
         {
-            State = Lose;
+            if( Reputation >= 100 )
+            {
+                State = Win;
+            }
+            else if( Reputation <= 0 )
+            {
+                State = Lose;
+            }
+            else
+            {
+                State = Observe;
+            }
         }
     }
 
